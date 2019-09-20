912b390f063fdef8424d3e5a8b12ba6c828c827b
Matt Yang <ericfromhust@gmail.com>
2019-09-20 16:40:58 -0400

    Update DominantColor.py

--- DominantColor.py
+++ DominantColor.py
@@ -15,7 +15,9 @@ def get_dominant_color(image, k=1):
     dominant_color_4 = [-1, -1, -1, -1]
     for item in [dominant_color_1, dominant_color_2, dominant_color_3, dominant_color_4]:
         item = [int(i) for i in item]
-        if (len(set(item[:3])) != 1) or (item[0] < 0):
+        value_diff = (abs(item[0]-item[1]) > 10) and ((abs(item[0]-item[2]) > 10)) \
+                     and ((abs(item[1]-item[2]) > 10))
+        if value_diff or (item[0] < 0):
             return item
         else:
             continue
