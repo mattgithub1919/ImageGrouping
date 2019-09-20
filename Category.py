b582a0461338511e0f3d14ae82d0fd8b8465b693
Matt Yang <ericfromhust@gmail.com>
2019-09-20 15:23:04 -0400

    Update Category.py

--- Category.py
+++ Category.py
@@ -4,4 +4,4 @@ def get_category(dominant_color):
     categories = ['red', 'green', 'blue', 'other']
     if dominant_color[0] < 0:
         return categories[3]
-    return categories[np.argmax(dominant_color)]
+    return categories[np.argmax(dominant_color[:3])]
