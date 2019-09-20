3787a727c7224e0bac1c5b72692b43c86228c254
Matt Yang <ericfromhust@gmail.com>
2019-09-20 14:35:05 -0400

    Update ReadImage.py

--- ReadImage.py
+++ ReadImage.py
@@ -3,14 +3,11 @@
 #   need to change to 3 (RGB)
 
 def read_image(image_filename):
-    global pixel_data
-    global image_group
     global filespath
     
     image_path = os.path.join(filespath, image_filename)
     # RGBA data obtained
-    image_color_RGBA = Image.open(image_path)
-    # Change RGBA data into RGB
-    # it's possible not to change. That will be a different solution
-    image_color = np.array(image_color_RGBA.convert('RGB'))
+    image_color = np.array(Image.open(image_path))
+    # image_color = np.array(image_color_RGBA.convert('RGB'))
     return image_color
+
