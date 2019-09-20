852f989220d5ebd12df662df57a6b3a1c1683068
Matt Yang <ericfromhust@gmail.com>
2019-09-20 13:36:20 -0400

    changed dominant color calculation method

--- DominantColor.py
+++ DominantColor.py
@@ -1,5 +1,5 @@
 # it returns the RGB value of the dominant color. if its first
-# and second dominant colors are grayscale, return [-1, -1, -1]
+# and second dominant colors are greyscale, return [-1, -1, -1, -1]
 
 # Please note the default k is set to 1, need to change the value
 # during function call
@@ -8,12 +8,13 @@ def get_dominant_color(image, k=1):
     clt = KMeans(n_clusters = k)
     labels = clt.fit_predict(image)
     label_counts = Counter(labels)
+    # one example of label_counts is [(0, 126230), (1, 61680), (2, 15040)]
     dominant_color_1 = clt.cluster_centers_[label_counts.most_common(1)[0][0]]
     dominant_color_2 = clt.cluster_centers_[label_counts.most_common(2)[1][0]]
-    if (dominant_color_1[0] == dominant_color_1[1]) & (dominant_color_1[0] == dominant_color_1[2]):
-        if (dominant_color_2[0] == dominant_color_2[1]) & (dominant_color_2[0] == dominant_color_2[2]):
-            return [-1, -1, -1]
+    dominant_color_3 = clt.cluster_centers_[label_counts.most_common(3)[2][0]]
+    dominant_color_4 = [-1, -1, -1, -1]
+    for i in [dominant_color_1, dominant_color_2, dominant_color_3, dominant_color_4]:
+        if (len(set(i[:3])) != 1) or (i[0] < 0):
+            return i
         else:
-            return dominant_color_2
-    else:
-        return dominant_color_1
+            continue
